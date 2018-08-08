7a307068d4d63abce43e01faaaa36a4aa24c6681
Anushree1997 <40564820+Anushree1997@users.noreply.github.com>
2018-08-08 20:47:58 +0530

    Recusive program to calculate factorial

---
+++ code/DAY1/recursive_fact.py
@@ -0,0 +1,15 @@
+#Write a recursive program to calculate factorial of a given number.
+
+
+
+
+num=int(input('Enter a number to calculate factorial:'))
+#below function uses recursion
+def fact(a):
+    if(a>0):
+       #while value of a is greater than 1
+       return (a*fact(a-1))
+    else:
+        #when it finally becomes 1
+        return 1
+print(fact(num))
