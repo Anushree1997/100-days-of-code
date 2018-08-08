d01ffee9a454d254bf4b77b2bca579343a4af804
Anushree1997 <40564820+Anushree1997@users.noreply.github.com>
2018-08-08 20:43:24 +0530

    Factorial without recursion

---
+++ code/DAY1/factorial_without_recusion.py
@@ -0,0 +1,10 @@
+
+#Write a program to calculate factorial without recursion.
+
+num=int(input('Enter a number to find factorial:'))
+fact=1
+#below repeats a loop each time the value of fact changes, finally saving the result to fact
+while(num>0):
+    fact*=num
+    num-=1
+print(fact)
